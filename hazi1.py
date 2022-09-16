1c9a682f3fc3fe3a723c2ed4c394d5e7c76fa5d1
nacsacsa <kolozsi.ocsi@gmail.com>
2022-09-16 17:40:03 +0200

    message log

---
+++ hazi1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf8
+
+def main():
+
+    mondat = input("Adjon meg egy mondatot: \n")
+    betuk = {}
+    for x in mondat:
+        betuk[x] = 0
+    for x in mondat:
+        betuk[x] += 1
+
+    print("Betűk gyakorisága: " , betuk)
+    mondat_forditva = mondat[::-1]
+    print("Fordítva: " , mondat_forditva)
+    szavak_listaja = []
+    szavak_listaja.extend(mondat.split(" "))
+    print("Listába helyezve szavanként: " , szavak_listaja)
+
+main()
---
+++ hazi2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf8
+
+def main():
+    print("Adjon meg egy számot és egy mértékegységet (cm/inch):")
+    szam = input()
+    mertek = input()
+    if mertek == "cm":
+        print(float(szam) * 2.54 , "inches")
+    elif mertek == "inch":
+        print(float(szam) / 2.54 , "cm")
+    else:
+        print("Not correct unit!")
+
+main()
