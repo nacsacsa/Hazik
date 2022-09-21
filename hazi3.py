0f7fb5e2dd63f736addce74e52c565c299a39fa4
nacsacsa <kolozsi.ocsi@gmail.com>
2022-09-21 22:12:15 +0200

    masodik het hazi

---
+++ hazi3.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf8
+
+def main():
+    print("Adja meg a háromyszög három oldalát cm-ben:")
+    a_oldal = int(input("a oldal (cm): "))
+    b_oldal = int(input("b oldal (cm): "))
+    c_oldal = int(input("c oldal (cm): "))
+    if c_oldal < a_oldal + b_oldal and b_oldal < a_oldal + b_oldal and a_oldal < b_oldal + c_oldal:
+        szoveg_igaz = "A {}, {} és {} oldalú háromszög szerkezhető."
+        print(szoveg_igaz.format(a_oldal, b_oldal, c_oldal))
+    else:
+        szoveg_hamis = "A {}, {} és {} oldalú háromszög NEM szerkezhető."
+        print(szoveg_hamis.format(a_oldal, b_oldal, c_oldal))
+
+if __name__ == "__main__":
+	main()
