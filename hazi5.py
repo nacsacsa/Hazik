c251da269ff766882127c70e4986bdb64d283045
nacsacsa <kolozsi.ocsi@gmail.com>
2022-10-07 17:34:58 +0200

    negyedik het hazi

--- hazi5.py
+++ hazi5.py
@@ -11,11 +11,14 @@ class Team():
     def __str__(self):
         return "{} a {}-en dolgozik {} szerepkörben.".format(self.nev,self.projekt,self.szerepkor)
 
+    def __eq__(self, other_dev):
+        return self.projekt == other_dev.projekt and self.nev != other_dev.nev
+
     def Ugyan_az_project():
         developers = [dev1, dev2, dev3 ,dev4]
         for dev in developers:
             for other_dev in developers:
-                if dev.projekt == other_dev.projekt and dev.nev != other_dev.nev:
+                if dev == other_dev:
                     print("{} és {} dolgoznak egy projekten.".format(dev.nev,other_dev.nev))
             developers.remove(dev)
 
